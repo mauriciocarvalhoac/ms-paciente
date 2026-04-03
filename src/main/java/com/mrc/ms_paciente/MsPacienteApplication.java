c1bdf40cae128e56e68e25bc0b61b929c87afc3e
SERVER\mauricio <mauriciocarvalhoac@gmail.com>
2026-04-03 12:14:00 -0300

    Commit inicial do projeto

---
+++ src/main/java/com/mrc/ms_paciente/MsPacienteApplication.java
@@ -0,0 +1,14 @@
+package com.mrc.ms_paciente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPacienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsPacienteApplication.class, args);
+		System.out.println("Projeto Executado com sucesso!");
+	}
+
+}
---
+++ src/test/java/com/mrc/ms_paciente/MsPacienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrc.ms_paciente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPacienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
